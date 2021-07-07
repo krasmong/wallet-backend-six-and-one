08c0f530134f53054e56242197fe9b71b4513637
Dm3583 <pankovdm@gmail.com>
2021-07-07 15:09:17 -0700

    [add-guard] feat: add guard

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,21 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const ctrl = require('../../../controllers/users')
-// const guard = require('../../helpers/guard')
+const guard = require('../../../helpers/guard')
 const {
   validateUserCreate,
   validateUserLogin,
-  validateMongoId
 } = require('./userValidation')
-
-// router.post('/signup', validateUser, ctrl.register)
-// router.post('/login', validateUser, ctrl.login)
-// router.post('/logout', guard, ctrl.logout)
-// router.get('/current', guard, ctrl.current)
 
 router.post('/signup', validateUserCreate, ctrl.register)
 router.post('/login', validateUserLogin, ctrl.login)
-router.post('/logout', validateMongoId, ctrl.logout)
-router.get('/current', validateMongoId, ctrl.current)
+router.post('/logout', guard, ctrl.logout)
+router.get('/current', guard, ctrl.current)
 
 module.exports = router
