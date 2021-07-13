d544f849ad679286bc324911a734e2cd057ab780
Oleksandr Pomalin <nilamopr@gmail.com>
2021-07-13 04:37:49 +0300

    adds calc balance

--- helpers/calculationBalance.js
+++ helpers/calculationBalance.js
@@ -1,7 +1,36 @@
-// const Transaction = require('../model/transaction')
+const Transaction = require('../model/transaction')
 
-// const getLastBalance = async (time_id, userId) => {
-//   const lastTransaction = await Transaction.find({
-//     time_id:
-//   })
-// }
+const getLastBalance = async (date, userId) => {
+  const lastTransaction = await Transaction.find({
+    date: { $lt: date },
+    owner: userId,
+  })
+    .sort({ date: -1 })
+    .limit(1)
+  if (!lastTransaction || lastTransaction.length === 0) {
+    return 0
+  } else return lastTransaction[0].balance
+}
+
+const calculationCurrentBalance = (balance, body) => {
+  if (body.type === 'DEPOSIT') {
+    return parseInt(balance + body.amount)
+  }
+  if (body.type === 'WITHDRAW') {
+    return parseInt(balance - body.amount)
+  }
+}
+
+const recalculationBalance = async (date, currentBalance, userId) => {
+  const balance = currentBalance
+  const transaction = await Transaction.find({
+    date: { $gt: date },
+    owner: userId,
+  })
+}
+
+module.exports = {
+  getLastBalance,
+  calculationCurrentBalance,
+  recalculationBalance,
+}
